82edd22036b4c77a573b5c2f767e4a89ebc2ab29
MSCorpProgramador02 <programador02@mscorp.group>
2018-12-04 12:56:58 -0600

    otras configuraciones

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const app = express();
-
+const path = require('path');
+const exphbs= require('express-handlebars');
 //Settings
-app.set('port', process.env.PORT || 3000)
+app.set('port', process.env.PORT || 3000);
+app.set('views',path.join(__dirname,'views'));
+app.engine('.hbs',exphbs({
+    defaultLayout:'',
+    layoutsDir:path.join(app.get('views'),'layouts'),
+    partialsDir:'',
+    extname:''
+}));
 //Middlewares
 
 //Global variables
