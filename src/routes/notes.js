e26019cd4109a80acb166db9a19d840ed72f45b5
MSCorpProgramador02 <programador02@mscorp.group>
2018-12-12 12:57:47 -0600

    Recepcion de datos

--- src/routes/notes.js
+++ src/routes/notes.js
@@ -1,6 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
+router.get('/notes/add',(req,res) => {
+    res.render('notes/new-note');
+});
+//Recepcionando datos
+router.post('/notes/new-note',(req,res)=>{
+    console.log(req.body)
+    res.send('ok');
+});
+
 router.get('/notes',(req,res) => {
     res.send('notes');
 });
