fe9d1c2a79ae05614d1d8746c1b7ff08c355b86e
westamz <westlymeza@gmail.com>
2018-12-12 22:03:17 -0600

    Datos validados

--- src/routes/notes.js
+++ src/routes/notes.js
@@ -6,8 +6,27 @@ router.get('/notes/add',(req,res) => {
 });
 //Recepcionando datos
 router.post('/notes/new-note',(req,res)=>{
-    console.log(req.body)
-    res.send('ok');
+    //en body vienen los valores enviados por post
+    //console.log(req.body)
+    const  {title,description} = req.body;
+    //validaciones de los campos envidos
+    const errors = [];
+    if(!title)
+    {
+        errors.push({text:'Please write a valid title'});
+    }
+    if(!description)
+    {
+        errors.push({text:'Please write a valid description'});
+    }
+    if(errors.length>0)
+    {
+        res.render('notes/new-note',{
+            errors,
+            title,
+            description
+        });
+    }
 });
 
 router.get('/notes',(req,res) => {
