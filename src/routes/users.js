1eb3175c1995cb5c716d5ef388fcefc862f6b1b2
westamz <westlymeza@gmail.com>
2018-12-26 23:01:40 -0600

    agrega, error en render

--- src/routes/users.js
+++ src/routes/users.js
@@ -25,22 +25,28 @@ router.post('/users/signup',async(req,res) => {
     }
     if(errors.length > 0)
     {
-        res.render('users/signup',{errors,name,email,password,confirm_password});
+        res.render('users/signup',{errors,name,email,password,confirm_password});   
     }
     else
     {
         //Validacion de correos repetidos
-        const emailUser  = User.findOne({email : email});
+        const emailUser  = await User.findOne({email});
+        console.log("Email User: \n",emailUser);
         if(emailUser )
         {
-            req.flash('error_msg','The email is already in use')
+            req.flash('error_msg',{text:'the email is already in use'});
+            res.render('users/signup',{name,email,password,confirm_password});
         }
-        const newUser = new User ({name,email,password});
-        console.log(newUser);
-        newUser.password = newUser.encryptPassword(password);
-        await newUser.save();
-        req.flash('success_msg','You are registered');
-        res.redirect('/users/signin')
+        else
+        {
+            const newUser = new User ({name,email,password});
+            //console.log(newUser);
+            newUser.password = await newUser.encryptPassword(password);
+            await newUser.save();
+            req.flash('success_msg','You are registered');
+            res.redirect('/users/signin');
+        }
+        
     }
     
     //res.render('users/signup');
